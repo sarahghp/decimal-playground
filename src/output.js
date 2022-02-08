1884fd97f7ccee8d2085485ac3904543947726b0
Sarah GHP <sarah.groff.palermo@gmail.com>
2022-02-08 16:24:50 -0500

    Fix hightlighting in output

--- src/output.js
+++ src/output.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import MonacoEditor from "react-monaco-editor";
 import { EDITOR_OPTIONS } from "./constants.js";
 
@@ -8,15 +8,22 @@ const editorOptions = {
 };
 
 const Output = ({ content, orderClass }) => {
-  const onEditorMounted = (editor) => {
-    console.log("Output mounted");
-  };
+  const outputEditor = useRef(null);
+
+  useEffect(() => {
+    outputEditor.current.editor.setSelection({
+      startLineNumber: 1,
+      startColumn: 1,
+      endLineNumber: 1,
+      endColumn: 1,
+    });
+  }, [content]);
 
   return (
     <div className={`editorWrapper ${orderClass}`}>
       <MonacoEditor
+        ref={outputEditor}
         options={editorOptions}
-        editorDidMount={onEditorMounted}
         language="javascript"
         value={content}
       />
