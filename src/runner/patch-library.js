1a9bd9fc797e75e43010dfe4e1d4adbcd22cd0c1
Sarah GHP <sarah.groff.palermo@gmail.com>
2022-05-18 16:28:43 +0200

    Adds not-equal ops

--- src/runner/patch-library.js
+++ src/runner/patch-library.js
@@ -21,3 +21,18 @@ export const typeofCheck = (arg) => {
 
   return typeof arg;
 };
+
+export const invertEquals = (left, right) => {
+  if (left instanceof Decimal128 || right instanceof Decimal128) {
+    return Decimal128(left).eq(right);
+  }
+
+  if (left instanceof Big || right instanceof Big) {
+    return Big(left).eq(right);
+  }
+
+  return left != right;
+};
+
+export const invertTypeCheckAndCallEq = (left, right) =>
+  !typeCheckAndCallEq(left, right);
